a22341f0e57ab02d35be53aea7dd8e4a27cf532d
shkolnick.kun@gmail.com <shkolnick.kun@gmail.com@7d5ddebd-cceb-662e-84b1-f62a66161674>
2011-11-19 15:12:18 +0000

    License change, production usage is enabled

--- kernel/sem_generic.c
+++ kernel/sem_generic.c
@@ -16,6 +16,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Please contact with me by E-mail: shkolnick.kun@gmail.com
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes BuguRTOS, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
 **************************************************************************/
 
 /*****************************************************************************************
