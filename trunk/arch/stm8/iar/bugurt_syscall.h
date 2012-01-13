5db72faed1fb2b27609a32b8ad951795e77ece82
shkolnick.kun@gmail.com <shkolnick.kun@gmail.com@7d5ddebd-cceb-662e-84b1-f62a66161674>
2012-01-13 18:06:44 +0000

    STM8 for IAR compiler pre release

---
+++ trunk/arch/stm8/iar/bugurt_syscall.h
@@ -0,0 +1,157 @@
+/**************************************************************************
+    BuguRTOS-0.3.x(Bugurt real time operating system)
+    Copyright (C) 2011  anonimous
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Please contact with me by E-mail: shkolnick.kun@gmail.com
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes BuguRTOS, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
+**************************************************************************/
+
+/*****************************************************************************************
+```````````````````````````````..:+oyhdmNNMMMMMMNmmdhyo+:..```````````````````````````````
+``````````````````````````.:oydNMMMMMMMMMNNNNNNNNMMMMMMMMMNdy+:.``````````````````````````
+```````````````````````:sdNMMMMMNdys+/::----------::/osydNMMMMMNds:```````````````````````
+```````````````````.+hNMMMMmho/-..........................-/shmMMMMNh/.```````````````````
+````````````````./hNMMMNho:...................................-:ohNMMMNh/`````````````````
+``````````````.smMMMNy/-.......:////ss++/+-.......................-+yNMMMmo.``````````````
+````````````-sNMMMd+-.....:////:-...-/s::..............+o/-..........-odMMMNs.````````````
+``````````.sNMMNh/....................................-o:--............./hMMMNo.``````````
+`````````+mMMMh:-........................................-...............-/dMMMm/`````````
+```````.yMMMm/...................-::--..................:-........--........+mMMMy.```````
+``````:mMMMy-.......................-::-...............-:.........-::........-yMMMd:``````
+`````/NMMN+-..................::-.....-:-............../--.........-:/-.......-+NMMN:`````
+````+NMMm::o/-..--/+o+o+++++o+osys+//::-::.............:/....-:-::-..-++-.......:NMMN/````
+```/NMMm:.-++-+yhs/-``       `dMMMMMmho+:-..............:-/+yho/:/+oyo//o-......./mMMN:```
+``-NMMm/....-/--::/o+o+oo+oo++oosyss+smyo/.............:yyho-`     smMMddh-......./NMMN.``
+`.hMMM/......-++:-.-:oooooooooosyyhhys/...............-+osyyyyysssssooo+ohhs-......+MMMh.`
+`+MMMy.......-::/++:-.....-:/++++o+o/--................--/oooo+o++o+++////-y/.......hMMM/`
+.mMMN:.........--:---...-::/+//::--.......................--//+//-...-.....+/......./NMMd`
+/MMMy............-::----------....----/+-.....................-++/:---....-o-........hMMN:
+yMMM/.............--:://///oo+++/+++oo+-.......................-://+/:--::::.........+MMMs
+mMMN:................----:/h:::/:/::---.........................--/osss+++:..........:NMMd
+MMMd-....................:mNs:-....................................--:odo:-..........:mMMN
+MMMd-....................yoshyoso:--................................../mm/...........:mMMM
+MMMm:..................../-o/h.y-:+dooo+/:--....................-/osydddmh...........:mMMN
+mMMN:........................+mNs `h.  -d/:/+h++ooooy+++s++y+++mo+y` yh.do...........:NMMh
+sMMM+........................-hmMo/ds  oMo`.-o     :h   s:`h` `Nysd.-Ny-h:...........+MMMo
+/MMMh........................./smMMMMd+NMMNNMh`    sN: .mNNMddNMMMMNmN+..............hMMN:
+.dMMN/........................./+hMMMMMMMMMMMMmhyyyNMNNMMMMMMMMMMMNsoo-............./NMMd`
+`+MMMh.........................-/+omMMMMMMMMMMMMMMMMMMMMMMMMMMMMms-/+...............hMMM/`
+`.hMMM+..........................:/-omMMMMMMMMMMMMMMMdmMdhMMMds/-..-...............oMMMy``
+``.NMMN/............................--/hNN/h.`ys:dmsd:-syos+--+.................../NMMN.``
+```/NMMm:...........................:+/--:+s++oo+osoo+/:-..-/+::-................/mMMN:```
+````/NMMN:............................-/++/:-..........-//+/-..:+.--............/NMMN/````
+`````:NMMN+-.............../+-.............-://////////:-.......+s+::.........-oNMMN:`````
+``````:mMMMy-..............:/o-.................................:yo//........-hMMMd-``````
+```````.yMMMm+.............:o:++-...............................+y+o-......-+mMMMs.```````
+`````````/mMMMd/-...........-++:+/-.---........................-ho+/.....-/dMMMm/`````````
+``````````.oNMMMh/............-++:++/////:....................-yo:o-...-+hMMMNo.``````````
+````````````.sNMMMdo-...........-++::++:-:/+//:..............:o:/o-..-omMMMNo.````````````
+``````````````.omMMMNy+-..........-/+-.:/+/:--:+++/++//:/::/+/-+/.-+hMMMMmo```````````````
+`````````````````/hNMMMNho:-...............-:/:-....--::::--..-/ohNMMMNy:`````````````````
+```````````````````./hNMMMMmhs/-..........................-/shNMMMMNy/.```````````````````
+```````````````````````:sdNMMMMMNdhso+/:----------:/+oshdNMMMMMNho:```````````````````````
+``````````````````````````.:+ydNMMMMMMMMMMNNNNNNMMMMMMMMMMmds+:```````````````````````````
+````````````````````````````````.:/osyhdmNNMMMMNNmdhyso/:.````````````````````````````````
+******************************************************************************************
+*                                                                                        *
+*                   This logo was graciously delivered by 10003-kun ITT:                 *
+*                                                                                        *
+*                           http://www.0chan.ru/r/res/9996.html                          *
+*                                                                                        *
+*****************************************************************************************/
+#ifndef _BUGURT_SYSCALL_H_
+#define _BUGURT_SYSCALL_H_
+
+extern const code_t syscall_routine[];
+
+void syscall( unsigned char num, void * arg );
+void do_syscall(void);
+
+///=================================================================
+///                 ОБРАБОТЧИКИ СИСТЕМНЫХ ВЫЗОВОВ
+///=================================================================
+///                   Управление процессами
+typedef struct {
+    proc_t * proc;
+    code_t pmain;
+    code_t sv_hook;
+    code_t rs_hook;
+    void * arg;
+    stack_t *sstart;
+    prio_t prio;
+    timer_t time_quant;
+    bool_t is_rt;
+} proc_init_arg_t;
+
+void scall_proc_init( void * arg );
+//----------------------------------------------------------------------
+typedef struct{
+    proc_t * proc;
+    bool_t scall_ret;
+}proc_runtime_arg_t;
+
+void scall_proc_run( void * arg );
+void scall_proc_restart( void * arg );
+void scall_proc_stop( void * arg );
+//----------------------------------------------------------------------
+void scall_proc_self_stop( void * arg );
+//----------------------------------------------------------------------
+void scall_proc_terminate( void * arg );
+//----------------------------------------------------------------------
+void scall_proc_flag_stop( void * arg );
+///=================================================================
+///                         Сигналы
+void scall_sig_init( void * arg );
+void scall_sig_wait( void * arg );
+void scall_sig_signal( void * arg );
+void scall_sig_broadcast( void * arg );
+///=================================================================
+///                         Семафоры
+typedef struct {
+    sem_t * sem;
+    count_t count;
+}sem_init_arg_t;
+void scall_sem_init( void * arg );
+//----------------------------------------------------------------------
+typedef struct {
+    sem_t * sem;
+    bool_t scall_ret;
+}sem_lock_arg_t;
+void scall_sem_lock( void * arg );
+//----------------------------------------------------------------------
+void scall_sem_unlock( void * arg );
+///=================================================================
+///                         Мьютексы
+typedef struct {
+    mutex_t * mutex;
+#ifdef CONFIG_USE_HIGHEST_LOCKER
+    prio_t prio;
+#endif // CONFIG_USE_HIGHEST_LOCKER
+}mutex_init_arg_t;
+void scall_mutex_init(void * arg);
+//----------------------------------------------------------------------
+typedef struct {
+    mutex_t * mutex;
+    bool_t scall_ret;
+} mutex_lock_arg_t;
+void scall_mutex_lock(void * arg);
+void scall_mutex_try_lock(void * arg);
+void scall_mutex_unlock(void * arg);
+#endif // _BUGURT_SYSCALL_H_
