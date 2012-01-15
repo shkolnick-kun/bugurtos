35cd816df41c45e986fdb5cc4dbad88d2b359468
shkolnick.kun@gmail.com <shkolnick.kun@gmail.com@7d5ddebd-cceb-662e-84b1-f62a66161674>
2012-01-15 14:51:20 +0000

    code cleanup

---
+++ arch/avr-gcc/config.h
@@ -0,0 +1,71 @@
+#ifndef _CONFIG_H_
+// Example config file
+#define _CONFIG_H_
+///==================================================================
+///               Don't edit this part of the file!!!
+///==================================================================
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#ifndef __ASSEMBLER__
+
+#define INLINE __attribute__((__always_inline__))
+#define WEAK __attribute__(( __weak__ ))
+
+#define SYS_TMR_ISR_ATTR __attribute__ (( signal, naked ))
+#define RESCHED_ISR_ATTR __attribute__ (( signal, naked ))
+
+#define NOP() __asm__ __volatile__("nop"::)
+#define SYSCALL_DELLAY() NOP();NOP();NOP()
+
+typedef unsigned char stack_t;
+
+///==================================================================
+///               Edit this part carefully!!!
+///==================================================================
+
+// Max priority levels are defined by index_t, 
+// you must specify BITS_IN_INDEX_T to show 
+// how many levels you actually are going to use.
+// data types
+typedef unsigned char index_t;
+#define BITS_IN_INDEX_T (8)
+
+// Even if index_t is unsigned long long, 
+// there will be only 64 priority levels available,
+// so unsigned char is allways enough.
+typedef unsigned char prio_t;
+
+// Unsigned char is enough.
+typedef unsigned char flag_t;
+
+// For AVR only 64Kib of RAM
+// may be available, so count_t can be 
+// unsigned char or unsigned short.
+// Unsigned short is enough.
+typedef unsigned short count_t;
+
+// You can specify any volatile unsigned type here.
+typedef volatile unsigned short timer_t;
+
+// Unsigned char is enough.
+// There is no reason to make it bigger.
+typedef unsigned char bool_t;
+
+///=================================================================
+//     BuguRTOS behavior compilation flags, edit carefully!!!
+///=================================================================
+#define CONFIG_USE_O1_SEARCH
+#define CONFIG_USE_HIGHEST_LOCKER
+#define CONFIG_HARD_RT
+
+///=================================================================
+///     Project stecific stuff, you are welcome to edit it!!!
+///=================================================================
+#define SYSTEM_TIMER_ISR TIMER2_COMPA_vect
+#define SYSCALL_ISR INT1_vect
+
+#endif //__ASSEMBLER__
+
+#endif //_CONFIG_H_
