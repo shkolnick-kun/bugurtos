7cde8fc519ff8d8cd9f137dffc3cf426d63f79ac
shkolnick.kun@gmail.com <shkolnick.kun@gmail.com@7d5ddebd-cceb-662e-84b1-f62a66161674>
2012-08-15 15:50:38 +0000

    Test work in progress Virtual SMP for avr added but not tested yet

---
+++ arch/avr/gcc-vsmp/bugurt_kernel.h
@@ -0,0 +1,155 @@
+/**************************************************************************
+    BuguRTOS-0.4.x(Bugurt real time operating system)
+    Copyright (C) 2011  anonimous
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Please contact with me by E-mail: shkolnick.kun@gmail.com
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes BuguRTOS, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
+**************************************************************************/
+
+/*****************************************************************************************
+```````````````````````````````..:+oyhdmNNMMMMMMNmmdhyo+:..```````````````````````````````
+``````````````````````````.:oydNMMMMMMMMMNNNNNNNNMMMMMMMMMNdy+:.``````````````````````````
+```````````````````````:sdNMMMMMNdys+/::----------::/osydNMMMMMNds:```````````````````````
+```````````````````.+hNMMMMmho/-..........................-/shmMMMMNh/.```````````````````
+````````````````./hNMMMNho:...................................-:ohNMMMNh/`````````````````
+``````````````.smMMMNy/-.......:////ss++/+-.......................-+yNMMMmo.``````````````
+````````````-sNMMMd+-.....:////:-...-/s::..............+o/-..........-odMMMNs.````````````
+``````````.sNMMNh/....................................-o:--............./hMMMNo.``````````
+`````````+mMMMh:-........................................-...............-/dMMMm/`````````
+```````.yMMMm/...................-::--..................:-........--........+mMMMy.```````
+``````:mMMMy-.......................-::-...............-:.........-::........-yMMMd:``````
+`````/NMMN+-..................::-.....-:-............../--.........-:/-.......-+NMMN:`````
+````+NMMm::o/-..--/+o+o+++++o+osys+//::-::.............:/....-:-::-..-++-.......:NMMN/````
+```/NMMm:.-++-+yhs/-``       `dMMMMMmho+:-..............:-/+yho/:/+oyo//o-......./mMMN:```
+``-NMMm/....-/--::/o+o+oo+oo++oosyss+smyo/.............:yyho-`     smMMddh-......./NMMN.``
+`.hMMM/......-++:-.-:oooooooooosyyhhys/...............-+osyyyyysssssooo+ohhs-......+MMMh.`
+`+MMMy.......-::/++:-.....-:/++++o+o/--................--/oooo+o++o+++////-y/.......hMMM/`
+.mMMN:.........--:---...-::/+//::--.......................--//+//-...-.....+/......./NMMd`
+/MMMy............-::----------....----/+-.....................-++/:---....-o-........hMMN:
+yMMM/.............--:://///oo+++/+++oo+-.......................-://+/:--::::.........+MMMs
+mMMN:................----:/h:::/:/::---.........................--/osss+++:..........:NMMd
+MMMd-....................:mNs:-....................................--:odo:-..........:mMMN
+MMMd-....................yoshyoso:--................................../mm/...........:mMMM
+MMMm:..................../-o/h.y-:+dooo+/:--....................-/osydddmh...........:mMMN
+mMMN:........................+mNs `h.  -d/:/+h++ooooy+++s++y+++mo+y` yh.do...........:NMMh
+sMMM+........................-hmMo/ds  oMo`.-o     :h   s:`h` `Nysd.-Ny-h:...........+MMMo
+/MMMh........................./smMMMMd+NMMNNMh`    sN: .mNNMddNMMMMNmN+..............hMMN:
+.dMMN/........................./+hMMMMMMMMMMMMmhyyyNMNNMMMMMMMMMMMNsoo-............./NMMd`
+`+MMMh.........................-/+omMMMMMMMMMMMMMMMMMMMMMMMMMMMMms-/+...............hMMM/`
+`.hMMM+..........................:/-omMMMMMMMMMMMMMMMdmMdhMMMds/-..-...............oMMMy``
+``.NMMN/............................--/hNN/h.`ys:dmsd:-syos+--+.................../NMMN.``
+```/NMMm:...........................:+/--:+s++oo+osoo+/:-..-/+::-................/mMMN:```
+````/NMMN:............................-/++/:-..........-//+/-..:+.--............/NMMN/````
+`````:NMMN+-.............../+-.............-://////////:-.......+s+::.........-oNMMN:`````
+``````:mMMMy-..............:/o-.................................:yo//........-hMMMd-``````
+```````.yMMMm+.............:o:++-...............................+y+o-......-+mMMMs.```````
+`````````/mMMMd/-...........-++:+/-.---........................-ho+/.....-/dMMMm/`````````
+``````````.oNMMMh/............-++:++/////:....................-yo:o-...-+hMMMNo.``````````
+````````````.sNMMMdo-...........-++::++:-:/+//:..............:o:/o-..-omMMMNo.````````````
+``````````````.omMMMNy+-..........-/+-.:/+/:--:+++/++//:/::/+/-+/.-+hMMMMmo```````````````
+`````````````````/hNMMMNho:-...............-:/:-....--::::--..-/ohNMMMNy:`````````````````
+```````````````````./hNMMMMmhs/-..........................-/shNMMMMNy/.```````````````````
+```````````````````````:sdNMMMMMNdhso+/:----------:/+oshdNMMMMMNho:```````````````````````
+``````````````````````````.:+ydNMMMMMMMMMMNNNNNNMMMMMMMMMMmds+:```````````````````````````
+````````````````````````````````.:/osyhdmNNMMMMNNmdhyso/:.````````````````````````````````
+******************************************************************************************
+*                                                                                        *
+*                   This logo was graciously delivered by 10003-kun ITT:                 *
+*                                                                                        *
+*                           http://www.0chan.ru/r/res/9996.html                          *
+*                                                                                        *
+*****************************************************************************************/
+#ifndef _BUGURT_KERNEL_H_
+#define _BUGURT_KERNEL_H_
+
+#include"../../../include/bugurt.h"
+
+// Конкатенация строк
+#define BUGURT_CONCAT(a,b) a##b
+
+// Пролог обработчика прерывания
+///                         АХТУНГ !!!
+/// Используется переменная saved_proc_sp для временного хранения
+/// указателя стека прерываемого процесса, если так не делать,
+/// компилятор будет стирать r16, r17 до сохранения контекста.
+#define BUGURT_ISR_START() \
+    saved_proc_sp = bugurt_save_context();\
+    kernel.sched.current_proc->spointer = saved_proc_sp;\
+    bugurt_set_stack_pointer( kernel.idle.spointer )
+// Выход из обработчика прерывания, восстановление контекста текущего процесса
+#define BUGURT_ISR_EXIT() \
+    bugurt_restore_context( kernel.sched.current_proc->spointer );\
+    __asm__ __volatile__("reti"::)
+
+// Эпилог обработчика прерывания
+#define BUGURT_ISR_END() \
+    bugurt_check_resched();\
+    BUGURT_ISR_EXIT()
+
+// Шаблон обработчика прерывания для внутреннего пользования
+#define _BUGURT_ISR(v,f) \
+__attribute__ (( signal, naked )) void v(void); \
+void v(void) \
+{ \
+    BUGURT_ISR_START();\
+    f();\
+    BUGURT_ISR_END();\
+} \
+
+/*
+Объявление обработчика прерывания.
+
+Обработка прерываний происходит в контексте main,
+он же контекст процесса холостого хода.
+
+Не очень быстро,
+зато память экономится.
+*/
+#define BUGURT_INTERRUPT(v) \
+void BUGURT_CONCAT(v,_func)(void);\
+_BUGURT_ISR(v,BUGURT_CONCAT(v,_func)) \
+void BUGURT_CONCAT(v,_func)(void)
+
+// Флаги состояния ядра
+#define KRN_FLG_RESCHED ((unsigned char)1)
+
+#ifdef SYSCALL_ISR
+#define KRN_FLG_DO_SCALL ((unsigned char)2)
+#endif // SYSCALL_ISR
+
+unsigned char kernel_state;
+//Временное хранилище для указателей стеков процессов.
+stack_t * saved_proc_sp;
+
+//Внешние функции, специфичные для AVR
+extern void start_scheduler( void );
+extern void stop_scheduler( void );
+#ifdef SYSCALL_ISR
+extern void raise_syscall_interrupt(void);
+#endif
+
+void bugurt_check_resched( void );
+
+extern stack_t * bugurt_save_context( void );
+extern void bugurt_restore_context( stack_t * new_sp );
+extern void bugurt_set_stack_pointer( stack_t * new_sp );
+extern stack_t * bugurt_reverse_byte_order ( stack_t * arg );
+
+#endif // _BUGURT_KERNEL_H_
---
+++ tests/vsmp-sandbox/config.h
@@ -0,0 +1,96 @@
+#ifndef _CONFIG_H_
+// Example config file
+#define _CONFIG_H_
+///==================================================================
+///               Don't edit this part of the file!!!
+///==================================================================
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#ifndef __ASSEMBLER__
+
+// Syscall table is allocated in FLASH
+#include <avr/pgmspace.h>
+#define SYSCALL_TABLE(a) const PROGMEM code_t a
+#define SYSCALL_TABLE_READ(a) (code_t)pgm_read_word(&a)
+// Another option is to allocate it in RAM
+//#define SYSCALL_TABLE(a) const code_t a
+//#define SYSCALL_TABLE_READ(a) a
+
+#define INLINE __attribute__((__always_inline__))
+#define WEAK __attribute__(( __weak__ ))
+
+#define SYS_TMR_ISR_ATTR __attribute__ (( signal, naked ))
+#define RESCHED_ISR_ATTR __attribute__ (( signal, naked ))
+
+#define NOP() __asm__ __volatile__("nop"::)
+#define SYSCALL_DELLAY() NOP();NOP();NOP()
+
+typedef unsigned char stack_t;
+
+///==================================================================
+///               Edit this part carefully!!!
+///==================================================================
+
+// Max priority levels are defined by index_t,
+// you must specify BITS_IN_INDEX_T to show
+// how many levels you actually are going to use.
+// data types
+typedef unsigned char index_t;
+#define BITS_IN_INDEX_T (8)
+
+// Even if index_t is unsigned long long,
+// there will be only 64 priority levels available,
+// so unsigned char is allways enough.
+typedef unsigned char prio_t;
+
+// Unsigned char is too small,
+// unsigned short is enough.
+typedef unsigned short flag_t;
+
+// For AVR only 64Kib of RAM
+// may be available, so count_t can be
+// unsigned char or unsigned short.
+// Unsigned short is enough.
+typedef unsigned short count_t;
+
+// You can specify any volatile unsigned type here.
+typedef volatile unsigned short timer_t;
+
+// Unsigned char is enough.
+// There is no reason to make it bigger.
+typedef unsigned char bool_t;
+
+// Unsigned char is enough.
+// There is no reason to make it bigger.
+typedef unsigned char syscall_t;
+
+// Unsigned short is enough
+// to handle data and pointers.
+// There is no reason to make it bigger
+// or smaller.
+typedef unsigned short ipc_data_t;
+
+typedef unsigned char core_id_t;
+typedef unsigned char affinity_t;
+typedef unsigned char lock_t;
+typedef unsigned char stat_t;
+typedef unsigned char load_t;
+///=================================================================
+//     BuguRTOS behavior compilation flags, edit carefully!!!
+///=================================================================
+#define CONFIG_USE_O1_SEARCH
+#define CONFIG_USE_HIGHEST_LOCKER
+#define CONFIG_HARD_RT
+#define CONFIG_MP
+#define MAX_CORES (3)
+
+///=================================================================
+///     Project stecific stuff, you are welcome to edit it!!!
+///=================================================================
+#define SYSTEM_TIMER_ISR TIMER2_COMPA_vect
+
+#endif //__ASSEMBLER__
+
+#endif //_CONFIG_H_
