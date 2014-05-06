da9d4e22697496d4afacdc0b336c29e263c95e29
shkolnick.kun@gmail.com <shkolnick.kun@gmail.com@7d5ddebd-cceb-662e-84b1-f62a66161674>
2014-05-06 12:00:11 +0000

    testing

--- tests/arch/stm8/raisonance/config.h
+++ tests/arch/stm8/raisonance/config.h
@@ -60,8 +60,7 @@ typedef unsigned long ipc_data_t;
 //=================================================================
 //     BuguRTOS behavior compilation flags, edit carefully!!!
 //=================================================================
-#define CONFIG_USE_O1_SEARCH
-#define CONFIG_USE_HIGHEST_LOCKER
+//#define CONFIG_USE_O1_SEARCH
 #define CONFIG_HARD_RT
 #define CONFIG_PREEMPTIVE_KERNEL
 #define CONFIG_USER_IDLE
