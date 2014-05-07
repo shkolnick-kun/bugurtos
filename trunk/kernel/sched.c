4f920341bd5b3b9dc89f0d5462bfcea742a519c7
shkolnick.kun@gmail.com <shkolnick.kun@gmail.com@7d5ddebd-cceb-662e-84b1-f62a66161674>
2014-05-07 06:27:29 +0000

    testing

--- trunk/kernel/sched.c
+++ trunk/kernel/sched.c
@@ -503,9 +503,9 @@ bool_t _sched_proc_yeld( void )
             SPIN_LOCK( proc );
 
             PROC_SET_STATE( proc, PROC_STATE_READY );
-#ifdef CONFIG_MP
+#if defined(CONFIG_MP) && defined(CONFIG_USE_ALB)
             sched = sched_stat_update_run( proc );
-#endif
+#endif // CONFIG_MP CONFIG_USE_ALB
             SPIN_LOCK( sched );
             gitem_insert( (gitem_t *)proc, sched->expired );
             SPIN_FREE( sched );
