5eff3fcaaad31172ca9ab4b30b506f1b2830ca0c
Anonimous <anon@legion>
2015-12-01 16:08:36 +0500

    Updated example config files.

---
+++ arch/stm8/raisonance/bugurt_config.h
@@ -0,0 +1,107 @@
+#ifndef _BGRT_CONFIG_H_
+/// This is example config file
+#define _BGRT_CONFIG_H_
+///==================================================================
+///               Don't edit this part of the file!!!
+///==================================================================
+
+#include <intrins.h>
+
+#define BGRT_SCL_TBL(a) bgrt_code_t code a
+#define BGRT_SCL_TBL_READ(a) a
+
+#define WEAK
+#define NOP _nop_
+#define bgrt_disable_interrupts _sim_
+#define bgrt_enable_interrupts _rim_
+
+typedef unsigned char bgrt_stack_t;
+
+///==================================================================
+///               Edit this part carefully!!!
+///==================================================================
+
+// Max priority levels are defined by bgrt_index_t,
+// you must specify BGRT_BITS_IN_INDEX_T to show
+// how many levels you actually are going to use.
+typedef unsigned char bgrt_index_t;
+#define BGRT_BITS_IN_INDEX_T (8)
+
+// Even if bgrt_index_t is unsigned long long,
+// there will be only 64 priority levels available,
+// so unsigned char is always enough.
+typedef unsigned char bgrt_prio_t;
+
+// Unsigned char is enough.
+typedef unsigned char bgrt_flag_t;
+// unsigned char is enough.
+typedef unsigned char bgrt_st_t;
+// For STM8 only 64Kib of stack space
+// may be available, so bgrt_cnt_t can be
+// unsigned char or unsigned short.
+// Unsigned short is enough.
+typedef unsigned short bgrt_cnt_t;
+
+// You can specify any unsigned type here.
+typedef unsigned short bgrt_tmr_t;
+
+// Unsigned char is enough.
+// There is no reason to make it bigger.
+typedef unsigned char bgrt_bool_t;
+
+// Unsigned char is enough.
+// There is no reason to make it bigger.
+typedef unsigned char bgrt_syscall_t;
+//=================================================================
+//     BuguRTOS behavior compilation flags, edit carefully!!!
+//=================================================================
+//#define BGRT_CONFIG_USE_O1_SEARCH
+#define BGRT_CONFIG_HARD_RT
+#define BGRT_CONFIG_PREEMPTIVE_KERNEL
+#define BGRT_CONFIG_USER_IDLE
+//=================================================================
+//     Project specific stuff, you are welcome to edit it!!!
+//=================================================================
+#define BGRT_CONFIG_TEST  //This is test project.
+#include <STM8\STM8S208RB.h>//STM8 include!!!!
+// project specific define of system timer ISR
+#define SPI_TXE_vector 10
+#define BGRT_SYSTEM_TIMER_VECTOR 23
+#define BGRT_SYSTEM_TIMER_INTERRUPT_CLEAR() (TIM4_SR = 0x00)
+#define BGRT_STOP_SCHEDULER() (TIM4_IER = 0x00)
+#define BGRT_START_SCHEDULER() (TIM4_IER = 0x01)
+
+#define BGRT_PROC_STACK_SIZE 128
+
+#define LOWEST (BGRT_BITS_IN_INDEX_T - 1)
+
+#define SVH0 (bgrt_code_t)0
+#define RSH0 (bgrt_code_t)0
+
+#define SVH1 (bgrt_code_t)0
+#define RSH1 (bgrt_code_t)0
+
+#define SVH2 (bgrt_code_t)0
+#define RSH2 (bgrt_code_t)0
+
+#define SVH3 (bgrt_code_t)0
+#define RSH3 (bgrt_code_t)0
+
+#define SVH4 (bgrt_code_t)0
+#define RSH4 (bgrt_code_t)0
+
+#define SVH5 (bgrt_code_t)0
+#define RSH5 (bgrt_code_t)0
+
+///==================================================================
+///               Don't edit this part of the file!!!
+///==================================================================
+
+extern void kernel_preemt_hook(void);
+#ifdef BGRT_CONFIG_PREEMPTIVE_KERNEL
+#define BGRT_KERNEL_PREEMPT() bgrt_enable_interrupts(); bgrt_disable_interrupts(); kernel_preemt_hook()
+#else // BGRT_CONFIG_PREEMPTIVE_KERNEL
+#define BGRT_KERNEL_PREEMPT() kernel_preemt_hook()
+#endif // BGRT_CONFIG_PREEMPTIVE_KERNEL
+
+#endif //_BGRT_CONFIG_H_
