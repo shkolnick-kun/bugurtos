a1bb3bc4b9566a1550ee16e1b7b9318f3316e0bf
shkolnick-kun <shkolnick.kun@gmail.com>
2016-01-09 02:47:32 +0500

    Working on lock free fifo.

--- tests/main/lock-free-fifo/main.c
+++ tests/main/lock-free-fifo/main.c
@@ -1,3 +1,82 @@
+/**************************************************************************
+    BuguRTOS-0.9.x (Bugurt real time operating system)
+    Copyright (C) 2015 anonimous
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Please contact with me by E-mail: shkolnick.kun@gmail.com
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes BuguRTOS, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
+**************************************************************************/
+
+/*****************************************************************************************
+```````````````````````````````..:+oyhdmNNMMMMMMNmmdhyo+:..```````````````````````````````
+``````````````````````````.:oydNMMMMMMMMMNNNNNNNNMMMMMMMMMNdy+:.``````````````````````````
+```````````````````````:sdNMMMMMNdys+/::----------::/osydNMMMMMNds:```````````````````````
+```````````````````.+hNMMMMmho/-..........................-/shmMMMMNh/.```````````````````
+````````````````./hNMMMNho:...................................-:ohNMMMNh/`````````````````
+``````````````.smMMMNy/-.......:////ss++/+-.......................-+yNMMMmo.``````````````
+````````````-sNMMMd+-.....:////:-...-/s::..............+o/-..........-odMMMNs.````````````
+``````````.sNMMNh/....................................-o:--............./hMMMNo.``````````
+`````````+mMMMh:-........................................-...............-/dMMMm/`````````
+```````.yMMMm/...................-::--..................:-........--........+mMMMy.```````
+``````:mMMMy-.......................-::-...............-:.........-::........-yMMMd:``````
+`````/NMMN+-..................::-.....-:-............../--.........-:/-.......-+NMMN:`````
+````+NMMm::o/-..--/+o+o+++++o+osys+//::-::.............:/....-:-::-..-++-.......:NMMN/````
+```/NMMm:.-++-+yhs/-``       `dMMMMMmho+:-..............:-/+yho/:/+oyo//o-......./mMMN:```
+``-NMMm/....-/--::/o+o+oo+oo++oosyss+smyo/.............:yyho-`     smMMddh-......./NMMN.``
+`.hMMM/......-++:-.-:oooooooooosyyhhys/...............-+osyyyyysssssooo+ohhs-......+MMMh.`
+`+MMMy.......-::/++:-.....-:/++++o+o/--................--/oooo+o++o+++////-y/.......hMMM/`
+.mMMN:.........--:---...-::/+//::--.......................--//+//-...-.....+/......./NMMd`
+/MMMy............-::----------....----/+-.....................-++/:---....-o-........hMMN:
+yMMM/.............--:://///oo+++/+++oo+-.......................-://+/:--::::.........+MMMs
+mMMN:................----:/h:::/:/::---.........................--/osss+++:..........:NMMd
+MMMd-....................:mNs:-....................................--:odo:-..........:mMMN
+MMMd-....................yoshyoso:--................................../mm/...........:mMMM
+MMMm:..................../-o/h.y-:+dooo+/:--....................-/osydddmh...........:mMMN
+mMMN:........................+mNs `h.  -d/:/+h++ooooy+++s++y+++mo+y` yh.do...........:NMMh
+sMMM+........................-hmMo/ds  oMo`.-o     :h   s:`h` `Nysd.-Ny-h:...........+MMMo
+/MMMh........................./smMMMMd+NMMNNMh`    sN: .mNNMddNMMMMNmN+..............hMMN:
+.dMMN/........................./+hMMMMMMMMMMMMmhyyyNMNNMMMMMMMMMMMNsoo-............./NMMd`
+`+MMMh.........................-/+omMMMMMMMMMMMMMMMMMMMMMMMMMMMMms-/+...............hMMM/`
+`.hMMM+..........................:/-omMMMMMMMMMMMMMMMdmMdhMMMds/-..-...............oMMMy``
+``.NMMN/............................--/hNN/h.`ys:dmsd:-syos+--+.................../NMMN.``
+```/NMMm:...........................:+/--:+s++oo+osoo+/:-..-/+::-................/mMMN:```
+````/NMMN:............................-/++/:-..........-//+/-..:+.--............/NMMN/````
+`````:NMMN+-.............../+-.............-://////////:-.......+s+::.........-oNMMN:`````
+``````:mMMMy-..............:/o-.................................:yo//........-hMMMd-``````
+```````.yMMMm+.............:o:++-...............................+y+o-......-+mMMMs.```````
+`````````/mMMMd/-...........-++:+/-.---........................-ho+/.....-/dMMMm/`````````
+``````````.oNMMMh/............-++:++/////:....................-yo:o-...-+hMMMNo.``````````
+````````````.sNMMMdo-...........-++::++:-:/+//:..............:o:/o-..-omMMMNo.````````````
+``````````````.omMMMNy+-..........-/+-.:/+/:--:+++/++//:/::/+/-+/.-+hMMMMmo```````````````
+`````````````````/hNMMMNho:-...............-:/:-....--::::--..-/ohNMMMNy:`````````````````
+```````````````````./hNMMMMmhs/-..........................-/shNMMMMNy/.```````````````````
+```````````````````````:sdNMMMMMNdhso+/:----------:/+oshdNMMMMMNho:```````````````````````
+``````````````````````````.:+ydNMMMMMMMMMMNNNNNNMMMMMMMMMMmds+:```````````````````````````
+````````````````````````````````.:/osyhdmNNMMMMNNmdhyso/:.````````````````````````````````
+******************************************************************************************
+*                                                                                        *
+*                   This logo was graciously delivered by 10003-kun ITT:                 *
+*                                                                                        *
+*                           http://www.0chan.ru/r/res/9996.html                          *
+*                                                                                        *
+*****************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,13 +84,13 @@ typedef int lf_st_t;
 #define LF_ST_OK (0)
 #define LF_ST_EAGAIN (1)
 //===============================================================================
-typedef struct _lf_item lf_item; //Lockfree list item;
+typedef struct _lf_item_t lf_item_t; //Lockfree list item;
 
-struct _lf_item
+struct _lf_item_t
 {
     volatile int valid;
-    lf_item * prev;
-    lf_item * next;
+    lf_item_t * prev;
+    lf_item_t * next;
 };
 
 //Default compare and swap implementation
@@ -23,26 +102,32 @@ struct _lf_item
 #define LF_YELD() do{}while(0)
 #endif
 
-void lf_item_init( lf_item * item );
-lf_st_t lf_item_put( lf_item * item, lf_item * fifo ); //Put an item to lock-free fifo (can used by multiple threads)
-lf_item * lf_item_get( lf_item * fifo ); //Get an item from lock-free fifo (MUST used by SINGLE thread only!!!)
+void lf_item_init( lf_item_t * item );
+lf_st_t lf_item_put( lf_item_t * item, lf_item_t * fifo ); //Put an item to lock-free fifo (can used by multiple threads)
+lf_item_t * lf_item_get( lf_item_t * fifo ); //Get an item from lock-free fifo (MUST used by SINGLE thread only!!!)
 //===============================================================================
-void lf_item_init( lf_item * item )
+void lf_item_init( lf_item_t * item )
 {
     item->prev = item;
     item->next = item;
     item->valid = 0xABCD;
 }
 //Put an item to lock-free fifo (can used by multiple threads)
-lf_st_t lf_item_put( lf_item * item, lf_item * fifo )
+///NOTE: THIS FUNCTION IS THREAD SAFE!!!
+///NOTE: THIS FUNCTION IS LOCK   FREE!!!
+///NOTE: THIS FUNCTION CAN BE USED IN REAL ISR!!!
+lf_st_t lf_item_put( lf_item_t * item, lf_item_t * fifo )
 {
+    //One itteration per interrupt on single processor.
     while(1)
     {
-        ///step 1a
+        ///step 1
         //Many threads may call to lf_item_put( item, some_fifo)
         if( LF_CAS( item->prev, item, fifo->prev) )
         {
             ///step 2
+            LF_CAS( item->next, item, fifo);
+            ///step 3
             //Many threads may call to lf_item_put( some_item, fifo)
             if( LF_CAS( fifo->prev, item->prev, item) )
             {
@@ -50,9 +135,9 @@ lf_st_t lf_item_put( lf_item * item, lf_item * fifo )
             }
             else
             {
-                //Undo item->prev = fifo->prev before next ettempt
-                ///step 1b
-                ///WARNING: No LF_YELD here, as lf_item_put must be called from REAL IRSs!!!
+                //Undo steps 2 and 1 on fail
+                //Undos are done in reverse order!!!
+                LF_CAS( item->next, item->next, item);
                 LF_CAS( item->prev, item->prev, item);
                 continue;
             }
@@ -63,42 +148,27 @@ lf_st_t lf_item_put( lf_item * item, lf_item * fifo )
             return LF_ST_EAGAIN;
         }
     }
-    /// step 3a
-    //OK! We are in the fifo;
-    //Now we must handle item->prev->next ASAP!
-    //Only two threads may write to item->prev->next, they are:
-    //this thread and the thread, that called lf_item_put(fifo->prev);
-    //So if lf_item_put(fifo->prev) finished, then item->prev->next == fifo
-    if( !LF_CAS( item->prev->next, fifo, item) )
-    {
-        ///step 3b
-        //Else item->prev->next should point to item->prev
-        if( !LF_CAS( item->prev->next, item->prev, item) )
-        {
-            //Other item->prev->next values are not valid!!!
-            while(1);
-        }
-    }
-    //The last, but not the least: we must handle item->next.
-    //If some other thread has interrupted this call, then item->next != item,
-    //so try to do item->next = fifo
-    ///step 4
-    LF_CAS( item->next, item, fifo);
+    //The last but not the list
+    //If item is not going to be geted, do...
+    LF_CAS( item->prev->next, fifo, item);
+
     return LF_ST_OK;
 }
 //Get an item from lock-free fifo (MUST used by SINGLE thread only!!!)
-/// WARNING: ONLY ONE THREAD IS ALLOWED CALL THIS FUNCTION!!!
-lf_item * lf_item_get( lf_item * fifo )
+/// WARNING: THIS FUNCTION IS NOT THREAD SAFE!!!
+/// WARNING: THIS FUNCTION IS NOT LOCK FREE!!!
+/// NOTE:    BUT IT'S FASTER THAN THREAD SAFE AND LOCKFREE!!!
+lf_item_t * lf_item_get( lf_item_t * fifo )
 {
     //Check if fifo is empty
     //Some thread may write fifo->prev at the same time, so...
     if( LF_CAS( fifo->prev, fifo, fifo) )
     {
-        return (lf_item *)0;
+        return (lf_item_t *)0;
     }
     else
     {
-        lf_item * head;
+        lf_item_t * head;
         //Ok, it's not empty, let's cut its head!
         //If other thread called lf_item_put( fifo->prev ),
         //then we must wait for step 3 to complite
@@ -110,41 +180,38 @@ lf_item * lf_item_get( lf_item * fifo )
         head = fifo->next;
         //head->prev won't be used until next lf_item_put, so...
         head->prev = head;
-        //If some thread called lf_item_put( head->prev ),
-        //then we must wait for step 3a/b to complete.
-        while( LF_CAS( head->next, head, head) )
+        //Check if there is only one item, and try to cut it with atomic operation.
+        if( LF_CAS( fifo->prev, head, fifo) )
         {
-            LF_YELD();
+            //There was only one item in the fifo
+            fifo->next = fifo;
         }
-        //If one more thread called lf_item_put( head->prev ),
-        if( !LF_CAS( fifo->prev, head, head) )
+        else
         {
             // wait for step 3b to complete.
             while( LF_CAS( head->next, fifo, fifo) )
             {
                 LF_YELD();
             }
-            //Only two threads may change head->next, so no more delays
+            //OK! All links have formed
+            head->next->prev = fifo;
+            fifo->next = head->next;
         }
-        //Here we can modify fifo->next, and head->next
-        fifo->next = head->next;
+        //Now we can handle head->next
         head->next = head;
-        //Now we must handle fifo->next->prev, it won't be used by ither threads!!
-        fifo->next->prev = fifo;
-
         return head;
     }
 }
 
 
-lf_item tst_item[10];
-lf_item tst_fifo;
+lf_item_t tst_item[10];
+lf_item_t tst_fifo;
 
 int main()
 {
     int i;
 
-    lf_item * tst_ret;
+    lf_item_t * tst_ret;
 
     (void)tst_ret;//Disable warning!
 
