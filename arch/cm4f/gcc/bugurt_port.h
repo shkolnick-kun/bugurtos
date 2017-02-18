b09354a4c7a9677b9660a90c014f7b4d7f5f6a3f
shkolnick-kun <shkolnick.kun@gmail.com>
2017-02-18 16:31:34 +0500

    Moving to atomic operations.

--- arch/cm4f/gcc/bugurt_port.h
+++ arch/cm4f/gcc/bugurt_port.h
@@ -32,9 +32,9 @@ static inline void bgrt_atm_init(bgrt_map_t * fic) /* ADLINT:SL:[W0629] linkage*
     BGRT_INT_FREE();
 }
 
-#define BGRT_ATM_BSET_ISR(map_ptr, msk) do{ *(map_ptr) |= (msk); }while(0)
+#define BGRT_ATM_BSET_ISR(map_ptr, msk) __atomic_fetch_or((map_ptr), (msk), __ATOMIC_SEQ_CST)
 
-static inline void bgrt_atm_bset(bgrt_map_t * fic, bgrt_map_t msk) /* ADLINT:SL:[W0629] linkage*/
+static inline void bgrt_atm_bset(bgrt_map_t * fic, bgrt_map_t msk)
 {
     BGRT_INT_LOCK();
     BGRT_ATM_BSET_ISR(fic,msk);
@@ -43,33 +43,16 @@ static inline void bgrt_atm_bset(bgrt_map_t * fic, bgrt_map_t msk) /* ADLINT:SL:
 
 #define BGRT_ATM_BGET_ISR(map_ptr, msk) (*(map_ptr) & (msk))
 
-static inline bgrt_map_t bgrt_atm_bget(bgrt_map_t * fic, bgrt_map_t msk) /* ADLINT:SL:[W0629] linkage*/
+static inline bgrt_map_t bgrt_atm_bget(bgrt_map_t * fic, bgrt_map_t msk)
 {
-    bgrt_map_t ret;
-    BGRT_INT_LOCK();
-    ret = BGRT_ATM_BGET_ISR(fic,msk);
-    BGRT_INT_FREE();
-    return ret; /* ADLINT:SL:[W0256,W0268] ret type check fail*/
+    return BGRT_ATM_BGET_ISR(fic,msk);
 }
 
-#define BGRT_ATM_BCLR_ISR(map_ptr, msk) (__bgrt_atm_bclr_isr((map_ptr), (msk)))
-static inline bgrt_map_t __bgrt_atm_bclr_isr(bgrt_map_t * fic, bgrt_map_t msk)
-{
-    bgrt_map_t ret;
-    //Get states
-    ret = *fic & msk; /* ADLINT:SL:[W0422,W0165] NULL ptr deref*/
-    //Clear states
-    *fic &= ~msk;     /* ADLINT:SL:[W0422,W0165,W0578] NULL ptr deref*/
-    return ret;           /* ADLINT:SL:[W0256,W0268] ret type check fail*/
-}
+#define BGRT_ATM_BCLR_ISR(map_ptr, msk) ((msk) & __atomic_fetch_and((map_ptr), ~(msk), __ATOMIC_SEQ_CST))
 
-static inline bgrt_map_t bgrt_atm_bclr(bgrt_map_t * fic, bgrt_map_t msk) /* ADLINT:SL:[W0629] linkage*/
+static inline bgrt_map_t bgrt_atm_bclr(bgrt_map_t * fic, bgrt_map_t msk)
 {
-    bgrt_map_t ret;
-    BGRT_INT_LOCK();
-    ret = BGRT_ATM_BCLR_ISR(fic,msk);
-    BGRT_INT_FREE();
-    return ret;         /* ADLINT:SL:[W0256,W0268] ret type check fail*/
+    return BGRT_ATM_BCLR_ISR(fic,msk);
 }
 
 #define BGRT_VINT_PUSH_ISR    bgrt_vint_push
