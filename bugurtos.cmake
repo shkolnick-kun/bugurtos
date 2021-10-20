40f5b85e9f2265d072f38b8ff5c4be076ff0c232
shkolnick-kun <shkolnick.kun@gmail.com>
2021-10-20 18:15:17 +0500

    Added basic CMAKE support.

---
+++ bugurtos.cmake
@@ -0,0 +1,102 @@
+cmake_minimum_required(VERSION 3.13)
+
+if (NOT TARGET _bugurtos_inclusion_guard)
+	add_library(_bugurtos_inclusion_guard INTERFACE)
+
+    message(NOTICE "BuguRTOS is included!!!")
+
+    if (NOT BGRT_CONFIG_DIR)
+		message(FATAL_ERROR "BGRT_CONFIG_DIR must be specified")
+	endif()
+    get_filename_component(BGRT_CONFIG_DIR "${BGRT_CONFIG_DIR}" REALPATH)
+
+	if (NOT BGRT_ARCH)
+		message(FATAL_ERROR "BGRT_ARCH must be specified")
+	endif()
+
+    if (NOT BGRT_TOOLCHAIN)
+		message(FATAL_ERROR "BGRT_TOOLCHAIN must be specified")
+	endif()
+
+    #Get current dir path
+    set(BGRT_DIR "${CMAKE_CURRENT_LIST_DIR}")
+	get_filename_component(BGRT_DIR "${BGRT_DIR}" REALPATH)
+
+    ###########################################################################
+    #                         Kernel source files
+    ###########################################################################
+    if (NOT BGRT_SYSCALL_TABLE_DIR)
+        set(BGRT_SYSCALL_TABLE_DIR "${CMAKE_CURRENT_LIST_DIR}/kernel/default")
+    endif()
+    get_filename_component(BGRT_SYSCALL_TABLE_DIR "${BGRT_SYSCALL_TABLE_DIR}" REALPATH)
+
+    #Kernel files
+    set(BGRT_KERNEL_SOURCES
+            ${BGRT_DIR}/kernel/crit_sec.c
+			${BGRT_DIR}/kernel/index.c
+			${BGRT_DIR}/kernel/item.c
+			${BGRT_DIR}/kernel/kernel.c
+			${BGRT_DIR}/kernel/pcounter.c
+			${BGRT_DIR}/kernel/pitem.c
+			${BGRT_DIR}/kernel/proc.c
+			${BGRT_DIR}/kernel/sched.c
+			${BGRT_DIR}/kernel/sync.c
+			${BGRT_DIR}/kernel/syscall.c
+			${BGRT_DIR}/kernel/timer.c
+			${BGRT_DIR}/kernel/vint.c
+			${BGRT_DIR}/kernel/xlist.c
+            )
+
+    #Port files
+    set(BGRT_PORT_DIR "${BGRT_DIR}/arch/${BGRT_ARCH}/${BGRT_TOOLCHAIN}")
+
+    list(APPEND BGRT_KERNEL_SOURCES ${BGRT_PORT_DIR}/bugurt_port.c)
+
+    if (BGRT_ARCH STREQUAL "avr")
+        list(APPEND BGRT_KERNEL_SOURCES ${BGRT_PORT_DIR}/bugurt_proc_stack_init.c)
+        list(APPEND BGRT_KERNEL_SOURCES ${BGRT_PORT_DIR}/bugurt_port_asm.S)
+
+    elseif (BGRT_ARCH STREQUAL "stm8")
+        #Stack init in separate file
+        list(APPEND BGRT_KERNEL_SOURCES ${BGRT_PORT_DIR}/bugurt_proc_stack_init.c)
+
+        #IAR port has asm file
+        if (BGRT_TOOLCHAIN STREQUAL "iar")
+            list(APPEND BGRT_KERNEL_SOURCES ${BGRT_PORT_DIR}/bugurt_port_asm.S)
+        endif()
+    endif()
+    
+    ###########################################################################
+    #                          Kernel include dirs
+    ###########################################################################
+    set(BGRT_KERNEL_INCLUDES
+			${BGRT_DIR}/kernel
+			${BGRT_SYSCALL_TABLE_DIR}
+            ${BGRT_PORT_DIR}
+            ${BGRT_CONFIG_DIR}
+            )
+
+    ###########################################################################
+    #                             Kernel library
+    ###########################################################################
+    add_library(bugurtos_kernel INTERFACE)
+	target_sources(bugurtos_kernel INTERFACE ${BGRT_KERNEL_SOURCES})
+	target_include_directories(bugurtos_kernel INTERFACE ${BGRT_KERNEL_INCLUDES})
+
+    ###########################################################################
+    #                           Native API library
+    ###########################################################################
+    if (BGRT_SYSCALL_TABLE_DIR STREQUAL "${CMAKE_CURRENT_LIST_DIR}/kernel/default")
+        add_library(bugurtos_native_api INTERFACE)
+        target_sources(bugurtos_native_api INTERFACE 
+                ${BGRT_DIR}/libs/native/cond.c
+                ${BGRT_DIR}/libs/native/ipc.c
+                ${BGRT_DIR}/libs/native/mutex.c
+                ${BGRT_DIR}/libs/native/sem.c
+                )
+        target_include_directories(bugurtos_native_api INTERFACE 
+                ${BGRT_KERNEL_INCLUDES}
+                ${BGRT_DIR}/libs/native
+                )
+    endif()
+endif()
